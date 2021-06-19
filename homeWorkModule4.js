373621f189d54642ee4f064290a28dad69faa742
Roman Korotaev <kro1986@ukr.net>
2021-06-19 22:45:06 +0300

    Module 4 ,  Task 7

--- homeWorkModule4.js
+++ homeWorkModule4.js
@@ -3,6 +3,8 @@
 
 
 //МОДУЛЬ 4
+
+
 /*  Задание 1
 
 Функция как значение
@@ -280,7 +282,7 @@ processCall('Поли', takeCall, leaveHoloMessage);
 
 */
 
-
+/*
 const pizzaPalace = {
   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
 
@@ -307,4 +309,294 @@ console.log ( pizzaPalace.order('Четыре нарезона', makePizza, onOr
 console.log (pizzaPalace.order('Биг майк', makePizza, onOrderError));
 console.log ( pizzaPalace.order('Венская', makePizza, onOrderError));
 
+*/
 
+
+
+
+//---------------------------------------------------------------------
+
+
+/*  Задание 5
+
+Контекст вызова функции
+Внутри функций есть зарезервированное ключевое слово this. Во время исполнения функции, в this записывается ссылка на объект, в контексте которого она была вызвана. Таким образом, в теле функции мы можем получить доступ к свойствам и методам этого объекта.
+
+const bookShelf = {
+  authors: ['Бернард Корнуэлл', 'Роберт Шекли'],
+  getAuthors() {
+    return this.authors;
+  },
+  addAuthor(authorName) {
+    this.authors.push(authorName);
+  },
+};
+
+console.log(bookShelf.getAuthors()); // ['Бернард Корнуэлл', 'Роберт Шекли']
+bookShelf.addAuthor('Ли Танит');
+console.log(bookShelf.getAuthors()); // ['Бернард Корнуэлл', 'Роберт Шекли', 'Ли Танит']
+Методы getAuthors и addAuthor это функции, которые вызываются в контексте объекта bookShelf. Во время их выполнения в this записывается ссылка на объект bookShelf и мы можем обратиться к его свойствам и методам.
+
+Задание
+Выполни рефакторинг методов объекта pizzaPalace, расставив отсутствующие this в местах обращения к свойствам и методам объекта.
+
+Тесты
+Вызов pizzaPalace.order('Аль Копчино') возвращает строку 'Заказ принят, готовим пиццу «Аль Копчино».'.
+Вызов pizzaPalace.order('Четыре нарезона') возвращает строку 'Заказ принят, готовим пиццу «Четыре нарезона».'.
+Вызов pizzaPalace.order('Биг майк') возвращает строку 'В ассортименте нет пиццы с названием «Биг майк».'.
+Вызов pizzaPalace.order('Венская') возвращает строку 'В ассортименте нет пиццы с названием «Венская».'.
+Метод checkPizza объекта pizzaPalace использует this.
+Метод order объекта pizzaPalace использует this.
+*/
+
+/*
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  // Пиши код ниже этой строки
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
+
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+  // Пиши код выше этой строки
+};
+
+*/
+
+
+
+
+//---------------------------------------------------------------------
+
+
+/*  Задание 6
+
+
+Задача. Аккаунт пользователя
+Задание
+Перед увольнением разработчик сломал исходный код управления аккаунтами пользователей нашего сервиса доставки еды. Выполни рефакторинг методов объекта customer, расставив отсутствующие this при обращении к свойствам объекта.
+
+После объявления объекта мы добавили вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
+
+Тесты
+Объявлена переменная customer.
+Значение переменной customer это объект со свойствами и методами.
+Вызов customer.getDiscount() возвращает текущее значение свойства discount.
+Вызов customer.setDiscount(0.15) обновляет значение свойства discount.
+Вызов customer.getBalance() возвращает текущее значение свойства balance.
+Вызов customer.getOrders() возвращает текущее значение свойства orders.
+Вызов customer.addOrder(5000, 'Steak') добавляет 'Steak' в массив значений свойства orders и обновляет баланс.
+Метод getBalance объекта customer использует this.
+Метод getDiscount объекта customer использует this.
+Метод setDiscount объекта customer использует this.
+Метод getOrders объекта customer использует this.
+Метод addOrder объекта customer использует this.
+
+*/
+
+/*
+
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Пиши код ниже этой строки
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return  this.discount;
+  },
+  setDiscount(value) {
+     this.discount = value;
+  },
+  getOrders() {
+    return  this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost *  this.discount;
+     this.orders.push(order);
+  },
+  // Пиши код выше этой строки
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+
+*/
+
+
+
+//---------------------------------------------------------------------
+
+
+/*  Задание 6
+
+Метод call
+Бывают ситуации когда функцию нужно вызвать в контексте какого-то объекта, при этом функция не является его методом. Для этого у функций есть методы call и apply.
+
+foo.call(obj, arg1, arg2, ...)
+Метод call вызовет функцию foo так, что в this будет ссылка на объект obj, а также передаст аргументы arg1, arg2 и т. д.
+
+function greetGuest(greeting) {
+  console.log(`${greeting}, ${this.username}.`);
+}
+
+const mango = { username: "Манго" };
+const poly = { username: "Поли" };
+
+greetGuest.call(mango, "Добро пожаловать");
+// Добро пожаловать, Манго.
+
+greetGuest.call(poly, "С приездом");
+// С приездом, Поли.
+Задание
+Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+
+Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. 
+Ваш заказ <позиция>-й в очереди.' Позиция это значение параметра position - позиция элемента в массиве
+ (на единицу больше чем индекс).
+
+Не объявляй дополнительные параметры функции composeMessage(position).
+Используй call для вызова функции в контексте одного объекта-заказа.
+Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
+Дополни код так, чтобы в переменной messages получился массив сообщений о статусе заказов из массива orders с помощью цикла for.
+
+Тесты
+Объявлена переменная orders.
+Значение переменной orders это исходный массив объектов-заказов.
+Объявлена функция composeMessage(position).
+У функции composeMessage объявлен один параметр position.
+В теле функции composeMessage используется this.
+Функция composeMessage вызывается методом call внутри цикла for.
+Объявлена переменная messages.
+Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
+*/
+
+
+
+
+//---------------------------------------------------------------------
+
+
+/*  Задание 7
+Метод call
+Бывают ситуации когда функцию нужно вызвать в контексте какого-то объекта, при этом функция не является его методом. Для этого у функций есть методы call и apply.
+
+foo.call(obj, arg1, arg2, ...)
+Метод call вызовет функцию foo так, что в this будет ссылка на объект obj, а также передаст аргументы arg1, arg2 и т. д.
+
+function greetGuest(greeting) {
+  console.log(`${greeting}, ${this.username}.`);
+}
+
+const mango = { username: "Манго" };
+const poly = { username: "Поли" };
+
+greetGuest.call(mango, "Добро пожаловать");
+// Добро пожаловать, Манго.
+
+greetGuest.call(poly, "С приездом");
+// С приездом, Поли.
+Задание
+Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+
+Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>.
+ Ваш заказ <позиция>-й в очереди.' Позиция это
+ значение параметра position - позиция элемента в массиве (на единицу больше чем индекс).
+
+Не объявляй дополнительные параметры функции composeMessage(position).
+Используй call для вызова функции в контексте одного объекта-заказа.
+Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
+Дополни код так, чтобы в переменной messages получился массив сообщений о статусе заказов из массива orders с помощью цикла for.
+
+Тесты
+Объявлена переменная orders.
+Значение переменной orders это исходный массив объектов-заказов.
+Объявлена функция composeMessage(position).
+У функции composeMessage объявлен один параметр position.
+В теле функции composeMessage используется this.
+Функция composeMessage вызывается методом call внутри цикла for.
+Объявлена переменная messages.
+Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
+
+*/
+
+
+/*
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+
+ return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+
+}
+
+const messages = [];
+
+for (let i = 0; i < orders.length; i += 1) {
+  //console.log (orders[i])
+ 
+  let tmpString = composeMessage.call(orders[i], i + 1)
+  messages.push(tmpString)
+ //messages.push( composeMessage.call(orders[i], i + 1) )
+}
+
+console.log (messages)
+
+*/
+
+
+
+//---------------------------------------------------------------------
+
+
+/*  Задание 8
+Метод apply
+Метод apply это аналог метода call за исключением того, что синтаксис передачи аргументов требует не перечисление, а массив, даже если аргумент всего один.
+
+foo.call(obj, arg1, arg2, ...)
+
+foo.apply(obj, [arg1, arg2, ...])
+Метод apply вызовет функцию foo так, что в this будет ссылка на объект obj, а также передаст элементы массива как отдельные аргументы arg1, arg2 и т. д. На практике, в основном используется метод call.
+
+function greetGuest(greeting) {
+  console.log(`${greeting}, ${this.username}.`);
+}
+
+const mango = { username: "Манго" };
+const poly = { username: "Поли" };
+
+greetGuest.apply(mango, ["Добро пожаловать"]); // Добро пожаловать, Манго.
+greetGuest.apply(poly, ["С приездом"]); // С приездом, Поли.
+Задание
+Выполни рефакторинг кода так, чтобы функция composeMessage(position) вызывалась методом apply.
+
+Тесты
+Объявлена переменная orders.
+Значение переменной orders это исходный массив объектов-заказов.
+Объявлена функция composeMessage(position).
+У функции composeMessage объявлен один параметр position.
+В теле функции composeMessage используется this.
+Функция composeMessage вызывается методом apply.
+Объявлена переменная messages.
+Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 
+'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
+
+*/
