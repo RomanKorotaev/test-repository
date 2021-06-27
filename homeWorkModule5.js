494a22d9dbac5343b4fba2005a2aa6711dd49f7c
Roman Korotaev <kro1986@ukr.net>
2021-06-27 22:25:51 +0300

    Mudule 5 Task 5

--- homeWorkModule5.js
+++ homeWorkModule5.js
@@ -355,3 +355,140 @@ const poly = new User({ name: 'Поли', email: 'poly@mail.com' });
 
 
 */
+
+/*
+
+function Car( {brand, model, price} ) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+
+
+*/
+
+
+
+//-----------------------------------------------------
+
+//  Задание 5
+
+/*
+Свойство prototype
+Мы уже знаем что такое прототип объекта, свойство __proto__ и как происходит поиск 
+отсутствующих свойств объекта по цепочке прототипов. Во время вызова функции-конструктора
+ через new и создания нового объекта со свойствами, ему также устанавливается прототип.
+
+У каждой функции, кроме стрелочных, есть свойство prototype, значение которого это 
+объект с единственным свойством constructor, указывающим на саму функцию-конструктор.
+
+function User() {}
+console.log(User.prototype); // { constructor: User }
+При вызове функции-конструктора и создании объекта через new, объект в свойстве 
+prototype функции-конструктора будет прототипом создаваемого объекта.
+
+const mango = new User();
+console.log(User.prototype.isPrototypeOf(mango)); // true
+В свойство prototype можно записывать свойства и методы, которые будут доступны 
+всем объектам созданным этой функцией-конструктором. Методы в prototype будут
+ вызываться объектами созданными функцией-конструктором, поэтому для доступа к 
+ свойствам вызываемого объекта в методах используется ключевое слово this.
+
+function User({ name, email }) {
+  this.name = name;
+  this.email = email;
+}
+
+User.prototype.getEmail = function () {
+  return this.email;
+};
+
+User.prototype.changeEmail = function (newEmail) {
+  this.email = newEmail;
+};
+
+const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+
+console.log(mango.getEmail()); // mango@mail.com
+mango.changeEmail('mango@supermail.com');
+console.log(mango.getEmail()); // mango@supermail.com
+Задание
+Добавь в свойство prototype функции-конструктора Car два метода:
+
+getPrice() - возвращает значение свойства price из объекта который его будет вызывать.
+changePrice(newPrice) - обновляет значение свойства price у объекта который его будет
+ вызывать на newPrice.
+Тесты
+Объявлена функция Car({ brand, model, price }).
+Вызов Car.prototype.hasOwnProperty('getPrice') возвращает true.
+Значение Car.prototype.getPrice это функция.
+Вызов Car.prototype.hasOwnProperty('changePrice') возвращает true.
+Значение Car.prototype.changePrice это функция.
+У объекта, созданного вызовом new Car({ brand: 'Audi', model: 'Q3', price: 36000 }),
+ вызов метода getPrice() вернет число 36000.
+У объекта, созданного вызовом new Car({ brand: 'Audi', model: 'Q3', price: 36000 }), 
+вызов метода changePrice(35000) и последующем вызове getPrice() вернет число 35000.
+
+*/
+
+/*
+
+function Car({ brand, model, price }) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+
+Car.prototype.getPrice = function () {
+ return this.price ;
+}
+
+Car.prototype.changePrice = function (newPrice) {
+ this.price=newPrice ;
+}
+ 
+
+*/
+
+
+
+//-----------------------------------------------------
+
+//  Задание 6
+/*
+Задача. Хранилище
+Задание
+С помощью Function Declaration напиши функцию-конструктор Storage, которая будет создавать
+ объекты для управления складом товаров. Функция ожидает только один аргумент - начальный
+  массив товаров, который записывается на создаваемый объект в свойство items.
+
+Добавь методы на прототип:
+
+getItems() - возвращает массив текущих товаров в свойстве items объекта, который вызывает этот метод.
+addItem(newItem) - принимает новый товар newItem и добавляет его в массив товаров в свойстве
+ items объекта, который
+ вызывает этот метод.
+removeItem(item) - принимает товар item и удаляет его из массива товаров в свойстве items
+ объекта, который вызывает этот метод.
+Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности,
+ в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
+
+Тесты
+Объявлена функция Storage(items).
+Вызов Storage.prototype.hasOwnProperty('getItems') возвращает true.
+Вызов Storage.prototype.hasOwnProperty('addItem') возвращает true.
+Вызов Storage.prototype.hasOwnProperty('removeItem') возвращает true.
+В результате вызова new Storage([ 'Нанитоиды', 'Пролонгер', 'Антигравитатор' ]) значение
+ переменной storage это объект.
+Вызов Storage.prototype.isPrototypeOf(storage) возвращает true.
+У объекта storage есть свойство items.
+Первый вызов storage.getItems(), сразу после инциализации экземпляра, возвращает
+ массив ["Нанитоиды", "Пролонгер", "Антигравитатор"].
+Второй вызов, storage.getItems(), после вызова storage.addItem('Дроид'), возвращает
+ массив ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"].
+Третий вызов storage.getItems(), после вызова storage.removeItem('Пролонгер'), возвращает
+ массив ["Нанитоиды", "Антигравитатор", "Дроид"].
+
+
+
+*/
