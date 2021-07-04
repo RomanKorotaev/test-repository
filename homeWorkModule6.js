950b0ccc2eadf2b2ead52c03602840c078e46480
Roman Korotaev <kro1986@ukr.net>
2021-07-04 17:08:28 +0300

    Module 6 Task 29

--- homeWorkModule6.js
+++ homeWorkModule6.js
@@ -2114,3 +2114,597 @@ const getInactiveUsers = (users) => {  return users.filter(user => user.isActive
 // Пиши код выше этой строки
 
 */
+
+
+
+//--------------------------------------------------
+
+
+/*  Задание 24
+
+Метод find()
+Если метод filter(callack) используется для поиска всех элементов удовлетворяющим условию, то метод find(callback) 
+позволяет найти и вернуть первый подходящий элемент, после чего перебор массива прекращается. То есть он ищет до первого совпадения.
+
+массив.find((element, index, array) => {
+  // Тело коллбек-функции
+});
+Не изменяет оригинальный массив.
+Поэлементно перебирает оригинальный массив.
+Возвращает первый элемент удовлетворяющий условию, то есть когда коллбек возвращает true.
+Если ни один элемент не подошёл, то есть для всех элементов коллбек вернул false, метод возвращает undefined.
+Метод find() используется для одной задачи - поиска элемента по уникальному значению свойства. Например, поиск пользователя
+ по почте, автомобиля по серийному номеру, книги по названию и т. п.
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+colorPickerOptions.find((option) => option.label === 'blue'); // { label: 'blue', color: '#2196F3' }
+colorPickerOptions.find((option) => option.label === 'pink'); // { label: 'pink', color: '#E91E63' }
+colorPickerOptions.find((option) => option.label === 'white'); // undefined
+Задание
+Используя метод find() дополни код так, чтобы:
+
+В переменной bookWithTitle получился объект книги название которой (свойство title) совпадает со значением переменной BOOK_TITLE.
+В переменной bookByAuthor получился объект книги автор который (свойство author) совпадает со значением переменной AUTHOR.
+Тесты
+Объявлена переменная books.
+Значение переменной books это массив.
+Объявлена переменная BOOK_TITLE.
+Значение переменной BOOK_TITLE это строка 'Сон смешного человека'.
+Объявлена переменная AUTHOR.
+Значение переменной AUTHOR это строка 'Роберт Шекли'.
+Объявлена переменная bookWithTitle.
+Значение переменной bookWithTitle это объект книги с названием выше 'Сон смешного человека'.
+Объявлена переменная bookByAuthor.
+Значение переменной bookByAuthor это объект книги автора 'Роберт Шекли'.
+Для перебора массива books использован метод find().
+*/
+
+/*
+const books = [
+  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+  { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+];
+const BOOK_TITLE = 'Сон смешного человека';
+const AUTHOR = 'Роберт Шекли';
+// Пиши код ниже этой строки
+
+const bookWithTitle = books.find( book => book.title===BOOK_TITLE);
+const bookByAuthor = books.find( book => book.author===AUTHOR);
+
+console.log('bookWithTitle = ', bookWithTitle);
+console.log('bookByAuthor = ', bookByAuthor);
+
+*/
+
+
+
+//--------------------------------------------------
+
+
+/*  Задание 25
+Задача. Пользователь с почтой
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male'
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female'
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male'
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female'
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male'
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male'
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female'
+  }
+]
+Задание
+Дополни функцию getUserWithEmail(users, email) так, чтобы она возвращала объект пользователя, почта которого (свойство email) 
+совпадает со значением параметра email.
+
+Тесты
+Объявлена функция getUserWithEmail(users, email).
+
+Для перебора параметра users используется метод find().
+
+Если значение параметра email это 'shereeanthony@kog.com', функция возвращает объект пользователя с именем Sheree Anthony.
+
+Если значение параметра email это 'elmahead@omatom.com', функция возвращает объект пользователя с именем Elma Head.
+
+Если значение параметра email это 'blackburndotson@furnigeer.com', функция возвращает объект пользователя с именем Blackburn Dotson.
+
+Если в массиве users нет пользователя с почтой из параметра email, функция возвращает undefined.
+
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+*/
+
+/*
+
+const userArray = 
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male'
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female'
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male'
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female'
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male'
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male'
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female'
+  }
+]
+
+
+function getUserWithEmail (users, email) {
+  return users.find( user => user.email===email)
+ }
+
+ console.log( getUserWithEmail (userArray, 'elmahead@omatom.com'));
+
+ */
+
+ 
+
+//--------------------------------------------------
+
+
+/*  Задание 26
+
+Метод every()
+Метод every(callback) проверяет проходят ли все элементы массива тест предоставляемый коллбек-функцией. Возвращает true или false.
+
+массив.every((element, index, array) => {
+  // Тело коллбек-функции
+});
+Не изменяет оригинальный массив.
+Поэлементно перебирает оригинальный массив.
+Возвращает true если все элементы массива удовлетворяют условию.
+Возвращает false если хотя бы один элемент массива не удовлетворяет условию.
+Перебор массива прекращается если коллбек возвращает false.
+// Все элементы больше либо равны нулю? - да
+[1, 2, 3, 4, 5].every((value) => value >= 0); // true
+
+// Все элементы больше либо равны нулю? - нет
+[1, 2, 3, -10, 4, 5].every((value) => value >= 0); // false
+При работе с массивом объектов проверяется значение какого-то их свойства.
+
+Задание
+Используя метод every() дополни код так, чтобы:
+
+В переменной eachElementInFirstIsEven был результат проверки всех элементов массива firstArray на чётность.
+В переменной eachElementInFirstIsOdd был результат проверки всех элементов массива firstArray на нечётность.
+В переменной eachElementInSecondIsEven был результат проверки всех элементов массива secondArray на чётность.
+В переменной eachElementInSecondIsOdd был результат проверки всех элементов массива secondArray на нечётность.
+В переменной eachElementInThirdIsEven был результат проверки всех элементов массива thirdArray на чётность.
+В переменной eachElementInThirdIsOdd был результат проверки всех элементов массива thirdArray на нечётность.
+Тесты
+Объявлена переменная firstArray.
+Значение переменной firstArray это массив [26, 94, 36, 18].
+Объявлена переменная secondArray.
+Значение переменной secondArray это массив [17, 61, 23].
+Объявлена переменная thirdArray.
+Значение переменной thirdArray это массив [17, 26, 94, 61, 36, 23, 18].
+Объявлена переменная eachElementInFirstIsEven.
+Значение переменной eachElementInFirstIsEven это буль true.
+Объявлена переменная eachElementInFirstIsOdd.
+Значение переменной eachElementInFirstIsOdd это буль false.
+Объявлена переменная eachElementInSecondIsEven.
+Значение переменной eachElementInSecondIsEven это буль false.
+Объявлена переменная eachElementInSecondIsOdd.
+Значение переменной eachElementInSecondIsOdd это буль true.
+Объявлена переменная eachElementInThirdIsEven.
+Значение переменной eachElementInThirdIsEven это буль false.
+Объявлена переменная eachElementInThirdIsOdd.
+Значение переменной eachElementInThirdIsOdd это буль false.
+Для перебора массивов использован метод every().
+*/
+
+/*
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Пиши код ниже этой строки
+
+const eachElementInFirstIsEven = firstArray.every ( value => value%2===0);
+const eachElementInFirstIsOdd = firstArray.every ( value => value%2!==0);
+
+const eachElementInSecondIsEven = secondArray.every ( value => value%2===0);
+const eachElementInSecondIsOdd = secondArray.every ( value => value%2!==0);
+
+const eachElementInThirdIsEven = thirdArray.every ( value => value%2===0);
+const eachElementInThirdIsOdd = thirdArray.every ( value => value%2!==0);
+
+console.log(eachElementInFirstIsEven)
+console.log(eachElementInFirstIsOdd)
+
+console.log(eachElementInSecondIsEven)
+console.log(eachElementInSecondIsOdd)
+
+console.log(eachElementInThirdIsEven)
+console.log (eachElementInThirdIsOdd)
+
+*/
+
+ 
+
+//--------------------------------------------------
+
+
+/*  Задание 27
+
+Задача. Все ли пользователи активны
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male'
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female'
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male'
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female'
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male'
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male'
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female'
+  }
+]
+Задание
+Дополни функцию isEveryUserActive(users) так, чтобы она проверяла все ли пользователи сейчас 
+активны (свойство isActive) и возвращала true или false.
+
+Тесты
+Объявлена переменная isEveryUserActive
+Переменной isEveryUserActive присвоена стрелочная функция с параметром (users).
+Для перебора параметра users используется метод every().
+Вызов функции с указанным массивом пользователей возвращает false.
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+
+*/
+
+/*
+// Пиши код ниже этой строки
+const isEveryUserActive = (users) => { return users.every ( user => user.isActive===true)
+   
+};
+// Пиши код выше этой строки
+
+*/
+
+
+ 
+
+//--------------------------------------------------
+
+
+/*  Задание 28
+
+Метод some()
+Метод some(callback) проверяет проходит ли хотя бы один элемент массива тест предоставляемый коллбек-функцией.
+ Возвращает true или false.
+
+массив.some((element, index, array) => {
+  // Тело коллбек-функции
+});
+Не изменяет оригинальный массив.
+Поэлементно перебирает оригинальный массив.
+Возвращает true если хотя бы один элемент массива удовлетворяет условию.
+Возвращает false если ни один элемент массив не удовлетворяет условию.
+Перебор массива прекращается если коллбек возвращает true.
+// Есть хотя бы один элемент больше либо равный нулю? - да
+[1, 2, 3, 4, 5].some(value => value >= 0); // true
+
+// Есть хотя бы один элемент больше либо равный нулю? - да
+[-7, -20, 3, -10, -14].some(value => value >= 0); // true
+
+// Есть хотя бы один элемент меньше нуля? - нет
+[1, 2, 3, 4, 5].some(value => value < 0); // false
+
+// Есть хотя бы один элемент меньше нуля? - да
+[1, 2, 3, -10, 4, 5].some(value => value < 0); // true
+Задание
+Используя метод some() дополни код так, чтобы:
+
+В переменной anyElementInFirstIsEven был результат проверки наличия чётных элементов в массиве firstArray.
+В переменной anyElementInFirstIsOdd был результат проверки наличия нечётных элементов в массиве firstArray.
+В переменной anyElementInSecondIsEven был результат проверки наличия чётных элементов в массиве secondArray.
+В переменной anyElementInSecondIsOdd был результат проверки наличия нечётных элементов в массиве secondArray.
+В переменной anyElementInThirdIsEven был результат проверки наличия чётных элементов в массиве thirdArray.
+В переменной anyElementInThirdIsOdd был результат проверки наличия нечётных элементов в массиве thirdArray.
+Тесты
+Объявлена переменная firstArray.
+Значение переменной firstArray это массив [26, 94, 36, 18].
+Объявлена переменная secondArray.
+Значение переменной secondArray это массив [17, 61, 23].
+Объявлена переменная thirdArray.
+Значение переменной thirdArray это массив [17, 26, 94, 61, 36, 23, 18].
+Объявлена переменная anyElementInFirstIsEven.
+Значение переменной anyElementInFirstIsEven это буль true.
+Объявлена переменная anyElementInFirstIsOdd.
+Значение переменной anyElementInFirstIsOdd это буль false.
+Объявлена переменная anyElementInSecondIsEven.
+Значение переменной anyElementInSecondIsEven это буль false.
+Объявлена переменная anyElementInSecondIsOdd.
+Значение переменной anyElementInSecondIsOdd это буль true.
+Объявлена переменная anyElementInThirdIsEven.
+Значение переменной anyElementInThirdIsEven это буль true.
+Объявлена переменная anyElementInThirdIsOdd.
+Значение переменной anyElementInThirdIsOdd это буль true.
+Для перебора массивов использован метод some().
+*/
+
+/*
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Пиши код ниже этой строки
+
+const anyElementInFirstIsEven = firstArray.some ( element => element%2===0);
+const anyElementInFirstIsOdd = firstArray.some ( element => element%2!==0);
+
+const anyElementInSecondIsEven = secondArray.some ( element => element%2===0);
+const anyElementInSecondIsOdd = secondArray.some ( element => element%2!==0);
+
+const anyElementInThirdIsEven = thirdArray.some ( element => element%2===0);
+const anyElementInThirdIsOdd = thirdArray.some ( element => element%2!==0);
+
+*/
+
+ 
+
+//--------------------------------------------------
+
+
+/*  Задание 29
+Задача. Есть ли активные пользователи
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male'
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female'
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male'
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female'
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male'
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male'
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female'
+  }
+]
+Задание
+Дополни функцию isAnyUserActive(users) так, чтобы она проверяла наличие активных пользователей (свойство isActive) 
+и возвращала true или false.
+
+Тесты
+Объявлена функция isAnyUserActive(users).
+Для перебора параметра users используется метод some().
+Вызов функции с указанным массивом пользователей возвращает true.
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+*/
+
+/*
+// Пиши код ниже этой строки
+const isAnyUserActive = users => {
+   return users.some (user => user.isActive === true)
+};
+// Пиши код выше этой строки
+
+*/
