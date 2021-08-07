2da940fd09f7a181b19c11f2447627d7994108f7
Roman Korotaev <kro1986@ukr.net>
2021-08-07 22:43:57 +0300

    Завершено тестирование Терминалом и  Parsel

--- tryNodeTerminal.js
+++ tryNodeTerminal.js
@@ -1,3 +1,14 @@
+const { required } = require("joi");
+
 console.log("Test NodeJS in Bush Terminal! :) ");
 
 const a=1;
+
+const Joi = require('joi');
+
+const passwordSchema = Joi.string().min(3).max(10).alphanum();
+
+console.log(passwordSchema.validate('W12345'));
+console.log ('Test')
+    
+ 
